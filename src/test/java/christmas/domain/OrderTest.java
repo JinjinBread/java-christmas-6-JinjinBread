8840e9286252b3c6cc738719ce6f4e95fe5cacc3
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-11 02:17:17 +0900

    test: Order 클래스의 총 주문 금액 계산 메서드 테스트 코드 작성

---
+++ src/test/java/christmas/domain/OrderTest.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void 총_주문_금액을_계산할_수_있다() {
+        Map<Menu, Integer> orderMenu = new EnumMap<>(Menu.class);
+        orderMenu.put(Menu.BARBECUE_RIBS, 1);
+        orderMenu.put(Menu.CHOCOLATE_CAKE, 2);
+        Order order = new Order(orderMenu);
+
+        int totalOrderAmount = order.calculateTotalOrderAmount();
+
+        assertThat(totalOrderAmount).isEqualTo(84_000);
+    }
+
+}
