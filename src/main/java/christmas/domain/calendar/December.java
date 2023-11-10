403a8178782ae07804856d19c488750d96c43976
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-11 02:12:00 +0900

    feat: date 검증 메서드 작성

--- src/main/java/christmas/domain/calendar/December.java
+++ src/main/java/christmas/domain/calendar/December.java
@@ -9,11 +9,14 @@ public class December {
     private static final int MONTH = 12;
     private static final int CHRISTMAS_DATE = 25;
 
+    private static final String INVALID_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
     private final int date;
     private final LocalDate localDate;
     private final DayOfWeek dayOfWeek;
 
     public December(int date) {
+        validateDateRange(date);
         this.date = date;
         this.localDate = LocalDate.of(YEAR, MONTH, date);
         this.dayOfWeek = localDate.getDayOfWeek();
@@ -35,6 +38,11 @@ public class December {
 
     public boolean isChristmasPeriod() {
         return localDate.getDayOfMonth() < 26;
+    }
+
+    private void validateDateRange(int date) {
+        if (date < 1 || date > 31)
+            throw new IllegalArgumentException(INVALID_DATE_ERROR_MESSAGE);
     }
 
 }
