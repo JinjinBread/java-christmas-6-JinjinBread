2d38c3750ee9084bc25ccb0145ab9e46149edd35
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-11 02:12:59 +0900

    test: Category 클래스 테스트 코드 작성

---
+++ src/test/java/christmas/domain/CategoryTest.java
@@ -0,0 +1,38 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void 메뉴의_카테고리_가져오기() {
+        Category categoryByMenu = Category.getCategoryByMenu(Menu.ICE_CREAM);
+
+        assertThat(categoryByMenu)
+                .isEqualTo(Category.DESSERT);
+    }
+
+    @Test
+    void 존재하지_않는_메뉴의_카테고리를_가져오면_예외() {
+        assertThrows(IllegalArgumentException.class, () -> Category.getCategoryByMenu(Menu.NONE));
+    }
+
+    @Test
+    void 메뉴의_카테고리가_맞는지_확인() {
+        boolean isCategoryMenu = Category.isCategoryMenu(Menu.T_BONE_STEAK, Category.MAIN);
+
+        assertThat(isCategoryMenu).isEqualTo(true);
+    }
+
+    @Test
+    void 메뉴의_카테고리가_아닌지_확인() {
+        boolean isCategoryMenu = Category.isCategoryMenu(Menu.T_BONE_STEAK, Category.DESSERT);
+
+        assertThat(isCategoryMenu).isEqualTo(false);
+    }
+
+}
