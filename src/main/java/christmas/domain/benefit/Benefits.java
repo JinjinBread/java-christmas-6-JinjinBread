7a75606620a43f1387693bb6a7d21b6bf1ba92dd
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-14 16:48:50 +0900

    refactor: 모든 혜택을 초기화 하는 메서드 작성

--- src/main/java/christmas/domain/benefit/Benefits.java
+++ src/main/java/christmas/domain/benefit/Benefits.java
@@ -1,26 +1,39 @@
 package christmas.domain.benefit;
 
-import christmas.domain.GiveawayMenu;
+import christmas.domain.Order;
+import christmas.domain.calendar.December;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Benefits {
 
-//    private final Discounts discounts;
-//    private final GiveawayEvent giveawayEvent;
+    private final December december;
+    private final Order order;
+    private final List<Benefit> benefits = new ArrayList<>();
 //    private final int totalDiscountAmount;
 //    private final int totalBenefitsAmount;
-//
-//    public Benefits(Discounts discounts, GiveawayEvent giveawayEvent) {
-//        this.discounts = discounts;
-//        this.giveawayEvent = giveawayEvent;
+
+    public Benefits(December december, Order order) {
+        this.december = december;
+        this.order = order;
+        initBenefits();
 //        this.totalDiscountAmount = calculateTotalDiscountAmount();
 //        this.totalBenefitsAmount = calculateTotalBenefitsAmount();
-//    }
-//
+    }
+
+    private void initBenefits() {
+        if (order.isMoreThan(10_000)) {
+            benefits.add(new ChristmasDiscount(december));
+            benefits.add(new WeekdayDiscount(december, order));
+            benefits.add(new WeekendDiscount(december, order));
+            benefits.add(new SpecialDiscount(december));
+            benefits.add(new GiveawayEvent(order));
+        }
+    }
+
 //    public List<GiveawayMenu> getGiveawayMenus() {
+//
 //        return giveawayEvent.getGiveawayMenus();
 //    }
 //
