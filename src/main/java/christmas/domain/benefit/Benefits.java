654f437e4a17f54f452665f1e4ce8940fabddd83
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-14 16:52:10 +0900

    refactor: 할인 금액 및 총 혜택 금액을 구하는 메서드 수정

--- src/main/java/christmas/domain/benefit/Benefits.java
+++ src/main/java/christmas/domain/benefit/Benefits.java
@@ -11,15 +11,15 @@ public class Benefits {
     private final December december;
     private final Order order;
     private final List<Benefit> benefits = new ArrayList<>();
-//    private final int totalDiscountAmount;
-//    private final int totalBenefitsAmount;
+    private final int totalDiscountAmount;
+    private final int totalBenefitsAmount;
 
     public Benefits(December december, Order order) {
         this.december = december;
         this.order = order;
         initBenefits();
-//        this.totalDiscountAmount = calculateTotalDiscountAmount();
-//        this.totalBenefitsAmount = calculateTotalBenefitsAmount();
+        this.totalDiscountAmount = calculateTotalDiscountAmount();
+        this.totalBenefitsAmount = calculateTotalBenefitsAmount();
     }
 
     private void initBenefits() {
@@ -32,32 +32,43 @@ public class Benefits {
         }
     }
 
+    public int getTotalDiscountAmount() {
+        return totalDiscountAmount;
+    }
+
+    public int getTotalBenefitsAmount() {
+        return totalBenefitsAmount;
+    }
+
+    private int calculateTotalDiscountAmount() {
+        int totalDiscountAmount = 0;
+        for (Benefit benefit : benefits) {
+            if (benefit.isGiveawayEvent()){
+                continue;
+            }
+            totalDiscountAmount += benefit.calculateBenefitAmount();
+        }
+        return totalDiscountAmount;
+    }
+
+    private int calculateTotalBenefitsAmount() {
+        int totalDiscountAmount = 0;
+        for (Benefit benefit : benefits) {
+            totalDiscountAmount += benefit.calculateBenefitAmount();
+        }
+        return totalDiscountAmount;
+    }
+
 //    public List<GiveawayMenu> getGiveawayMenus() {
 //
 //        return giveawayEvent.getGiveawayMenus();
 //    }
-//
-//    public int getTotalDiscountAmount() {
-//        return totalDiscountAmount;
-//    }
-//
-//    public int getTotalBenefitsAmount() {
-//        return totalBenefitsAmount;
-//    }
-//
+
 //    public Map<String, Integer> getBenefitsDetails() {
 //        Map<String, Integer> totalBenefits = new LinkedHashMap<>();
 //        discounts.getTotalDiscountDetails(totalBenefits);
 //        giveawayEvent.getGiveawayDetails(totalBenefits);
 //        return totalBenefits;
-//    }
-//
-//    private int calculateTotalDiscountAmount() {
-//        return discounts.calculateTotalDiscountAmount();
-//    }
-//
-//    private int calculateTotalBenefitsAmount() {
-//        return calculateTotalDiscountAmount() + giveawayEvent.calculateGiveawayPrice();
 //    }
 
 }
