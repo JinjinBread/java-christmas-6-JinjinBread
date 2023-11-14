e47f10d1638dfe04e598540ebea6f75a3ca26821
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-14 18:55:58 +0900

    feat: PlannerController 클래스 생성 및 December, Order 생성 메서드 구현

---
+++ src/main/java/christmas/controller/PlannerController.java
@@ -0,0 +1,76 @@
+package christmas.controller;
+
+import christmas.domain.calendar.December;
+import christmas.domain.menu.Menu;
+import christmas.domain.order.Order;
+import christmas.service.PlannerService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class PlannerController {
+
+    private static final String FIRST_DELIMITER = ",";
+    private static final String SECOND_DELIMITER = "-";
+
+    private static final OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final PlannerService plannerService = new PlannerService();
+
+    public void startPlanner() {
+
+        outputView.printIntroduce();
+
+    }
+
+    private December generateDecember() {
+        try {
+            int visitDate = inputView.readDate();
+            return new December(visitDate);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return generateDecember();
+        }
+    }
+
+    private Order generateOrder() {
+        try {
+            String stringOrderMenu = inputView.readOrderMenu();
+            Map<Menu, Integer> orderMenu = convertStringToMap(stringOrderMenu);
+            return new Order(orderMenu);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return generateOrder();
+        }
+    }
+
+    private Map<Menu, Integer> convertStringToMap(String stringOrderMenu) {
+        Map<Menu, Integer> orderMenu = new EnumMap<>(Menu.class);
+
+        String[] firstSplitStringOrderMenu = stringOrderMenu.split(FIRST_DELIMITER, -1);
+        // 메뉴 형식 검증
+        // Empty가 포함돼 있으면 error (e.g. 해산물파스타-2,레드와인-1,,,)
+        for (String splitStringOrderMenu : firstSplitStringOrderMenu) {
+            // 메뉴 형식 검증
+            // 1. SECOND_DELIMITER로 나눠서 나온 String 배열의 size가 2가 아니거나 (e.g. 해산물파스타2)
+            // 2. Empty가 포함돼 있으면 error (e.g. 해산물파스타-2----)
+            String[] secondSplitStringOrderMenu = splitStringOrderMenu.split(SECOND_DELIMITER, -1);
+            String menuName = secondSplitStringOrderMenu[0];
+            String menuCount = secondSplitStringOrderMenu[1];
+
+            // 유효한 메뉴인지 검증
+            // 메뉴의 개수가 숫자이며, 1 이상인지 검증
+            orderMenu.put(Menu.getMenu(menuName), Integer.parseInt(menuCount));
+        }
+        // 중복 메뉴가 입력됐는지 검증
+        // 음료만 주문했는지 검증
+        // 메뉴의 개수가 20개가 초과됐는지 검증
+
+        return orderMenu;
+    }
+
+
+
+}
